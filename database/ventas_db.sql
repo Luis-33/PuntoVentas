e5708b9c8a8ec0a89ac3c7e8b299b3d2d8b4612f
Gabriel <gabripalaci334@gmail.com>
2026-02-03 20:22:50 -0600

    Terminacion de Base de Datos

--- database/ventas_db.sql
+++ database/ventas_db.sql
@@ -30,5 +30,44 @@ CREATE TABLE productos (
 );
 
 CREATE TABLE ventas(
-    
-):
+    id_ventas SERIAL PRIMARY KEY,
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total NUMERIC(10,2) NOT NULL 0,
+    id_usuario INT NOT NULL,
+    CONSTRAINT fk_usuario
+        FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id_usuario)
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE detalle_ventas(
+    id_detalle_ventas SERIAL PRIMARY KEY,
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cantidad INT NOT NULL CHECK (cantidad > 0),
+    precio_detalle_ventas NUMERIC(10,2) NOT NULL,
+    subtotal_ventas NUMERIC(10,2) NOT NULL,
+    id_producto INT NOT NULL,
+    CONSTRAINT fk_producto
+        FOREIGN KEY (id_producto)
+        REFERENCES productos(id_producto)
+        ON DELETE RESTRICT
+    id_ventas INT NOT NULL,
+    CONSTRAINT fk_ventas
+        FOREIGN KEY (id_ventas)
+        REFERENCES ventas(id_ventas)
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE inventario(
+    id_inventario SERIAL PRIMARY KEY,
+    tipo VARCHAR(20) NOT NULL,
+    cantidad_producto INT NOT NULL CHECK (cantidad_producto > 0),
+    comentarios TEXT,
+    fecha_inventario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_producto INT NOT NULL,
+    CONSTRAINT fk_producto
+        FOREIGN KEY (id_producto)
+        REFERENCES productos(id_producto)
+        ON DELETE RESTRICT
+);
+
